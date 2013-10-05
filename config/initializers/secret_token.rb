3f001b8526683cc0874da75f09d1c71952662fb5
jleo3 <joseph.leo3@gmail.com>
2013-10-05 10:02:23 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gtr::Application.config.secret_key_base = '70fce7e02132534a1f6b7b86ec68f2a2eb41110f6d7810c6b5b04b488defb9e64857fb8c364849aa63220d4e659c1478e049053f4d5d5042645d895c816b75f2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gtr::Application.config.session_store :cookie_store, key: '_gtr_session'
