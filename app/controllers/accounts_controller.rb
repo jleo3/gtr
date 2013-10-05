cb5ae337d70d1a0b90b8844bfd8b3b1dd0640afb
jleo3 <joseph.leo3@gmail.com>
2013-10-05 18:45:04 -0400

    one failing test

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -2,4 +2,7 @@ class AccountsController < ApplicationController
   def show
     @account = Account.find(params[:id])
   end
+
+  def update
+  end
 end
--- spec/controllers/accounts_controller_spec.rb
+++ spec/controllers/accounts_controller_spec.rb
@@ -2,13 +2,24 @@ require 'spec_helper'
 
 describe AccountsController do
 
-  describe "GET 'show'" do
+  before do
+    @account = Account.make!
+  end
 
+  describe "GET 'show'" do
     it "should be successful" do
-      get :show, :id => Account.make!.id
+      get :show, :id => @account.id
       response.should be_success
     end
+  end
 
+  describe "POST 'update'" do
+    it "should update balance" do
+      xhr :post, :update, id: @account
+
+      response.should be_success
+      @account.balance.should eq(900)
+    end
   end
 
 end
