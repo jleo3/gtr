f5a40b7374188eaeabaa899f8dd2a0a786ec6d01
jleo3 <joseph.leo3@gmail.com>
2013-10-14 13:00:31 -0400

    show account name for credits

--- spec/features/credits_feature_spec.rb
+++ spec/features/credits_feature_spec.rb
@@ -4,7 +4,8 @@ describe "when I visit the credits index page" do
   it "will show me all of the credits" do
     @credit = Credit.make! creditor: "Gregory's",
                            amount: 2.40,
-                           purchase_date: DateTime.parse("October 7th, 2013")
+                           purchase_date: DateTime.parse("October 7th, 2013"),
+                           account: Account.make!
                            
 
     visit '/credits'
@@ -12,5 +13,6 @@ describe "when I visit the credits index page" do
     page.should have_content @credit.creditor
     page.should have_content @credit.amount
     page.should have_content @credit.purchase_date.strftime("%a %e %b %l:%M %p")
+    page.should have_content @credit.account.name
   end
 end
