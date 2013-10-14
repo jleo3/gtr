fd4557255dc8ad6c1f103674b753e0f35f4a903d
jleo3 <joseph.leo3@gmail.com>
2013-10-14 13:26:27 -0400

    updating seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,11 @@ user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => EN
 puts 'user: ' << user.name
 user.confirm!
 
-Account.create! name: "Checking", balance: 1000.0
+account = Account.create! name: "Checking", balance: 1000.0
 
 date = DateTime.parse("October 7th, 2013")
-Credit.create! creditor: "Gregory's", amount: 2.40, purchase_date: date, processed_date: date
+credit = Credit.create! creditor: "Gregory's", amount: 2.40, purchase_date: date, processed_date: date
+debit = Debit.create! debtor: "Credit Suisse", amount: 3500.00, debit_date: date, processed_date: date
+
+account.credits << credit
+account.save!
