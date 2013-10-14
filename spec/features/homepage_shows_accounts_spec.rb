56ab08aac921c2a3042fc2a5b5acef66e2c13426
jleo3 <joseph.leo3@gmail.com>
2013-10-14 12:03:32 -0400

    adding a link to credits on the homepage

--- spec/features/homepage_shows_accounts_spec.rb
+++ spec/features/homepage_shows_accounts_spec.rb
@@ -3,20 +3,25 @@ require 'spec_helper'
 describe "when I hit the homepage", :type => :feature do
   before do
     @account = Account.make! name: "Checking", balance: 1000
+    visit "/"
   end
 
   it "shows my checking account balance" do
-    visit "/"
-
-    expect(page).to have_content "Balance"
-    expect(page).to have_content "1000"
+    page.should have_content "Balance"
+    page.should have_content "1000"
   end
 
   it "links to account show page" do
-    visit "/"
     click_link @account.name
 
-    expect(page).to have_content "Account Information"
-    expect(page).to have_content @account.name
+    page.should have_content "Account Information"
+    page.should have_content @account.name
+  end
+
+  it "can navigate to credits index" do
+
+    click_link "Credits"
+
+    page.should have_content "Creditor"
   end
 end
