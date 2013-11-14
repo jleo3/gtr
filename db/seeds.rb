487b206b7d3efde6a170d02e47e41a7798f9b20a
jleo3 <joseph.leo3@gmail.com>
2013-11-14 13:49:56 -0500

    don't use confirm method; just save

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 puts 'DEFAULT USERS'
 user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
 puts 'user: ' << user.name
-user.confirm!
+user.save!
 
 account = Account.create! name: "Checking", balance: 1000.0
 
