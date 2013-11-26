cbe3e504729ebbd0f40a7c999b9246a66630c194
jleo3 <joseph.leo3@gmail.com>
2013-11-25 19:30:04 -0500

    Update seeds file no more confirm! just save! add account: to Debit and Credit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,13 @@
 puts 'DEFAULT USERS'
 user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
 puts 'user: ' << user.name
-user.confirm!
+user.save!
 
 account = Account.create! name: "Checking", balance: 1000.0
 
 date = DateTime.parse("October 7th, 2013")
-credit = Credit.create! creditor: "Gregory's", amount: 2.40, purchase_date: date, processed_date: date
-debit = Debit.create! debtor: "Credit Suisse", amount: 3500.00, debit_date: date, processed_date: date
+credit = Credit.create! creditor: "Gregory's", amount: 2.40, purchase_date: date, processed_date: date, account: account
+debit = Debit.create! debtor: "Credit Suisse", amount: 3500.00, debit_date: date, processed_date: date, account: account
 
 account.credits << credit
 account.debits << debit
